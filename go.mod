40fd538791071ead68e6377cabf1a5d1927fa366
cobckm <cole@cobckm.com>
2026-04-06 14:38:12 -0500

    feat: Setup http server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cobckm/cobckm.com
 
 go 1.26.1
+
+require github.com/go-chi/chi/v5 v5.2.5 // indirect
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Under Construction"))
+	})
+
+	srv := &http.Server{
+		Addr:         ":3000",
+		Handler:      r,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+
+}
